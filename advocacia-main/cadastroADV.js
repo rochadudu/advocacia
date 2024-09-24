8700ed15600b2470d97630224f2279c661995aba
rochadudu <128058865+rochadudu@users.noreply.github.com>
2024-09-24 08:10:12 -0300

    Delete advocacia-main directory

--- advocacia-main/cadastroADV.js
+++
@@ -1,7 +0,0 @@
-function cadastra_aluno() {
-    const no_al= document.getElementById('nome_aluno').value;
-    const dn_al= document.getElementById('data_nascimento').value;
-    const cg_al= document.getElementById('cgm').value;
-
-    document.getElementById('mostra_nome').innerText= no_al:
-}
